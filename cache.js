1c4be482a3c8239dcd3da9ed89754c9893119a73
fibo <casati_gianluca@yahoo.it>
2018-07-06 17:38:11 +0200

    log errors, fixed filename

--- cache.js
+++ cache.js
@@ -2,7 +2,7 @@ const CACHE_NAME = 'inversive.space-v1'
 
 const CACHED_URLS = [
   '/',
-  '/animate.js',
+  '/animation.js',
   '/cache.js',
   '/manifest.json',
   '/logo/512x512.png'
@@ -10,15 +10,17 @@ const CACHED_URLS = [
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(CACHED_URLS))
+    caches.open(CACHE_NAME).then(
+      cache => cache.addAll(CACHED_URLS)
+    ).catch(error => { console.error(error) })
   )
 })
 
 // Cache falling back to the network.
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request)
-    })
+    caches.match(event.request).then(
+      response => response || fetch(event.request)
+    ).catch(error => { console.error(error) })
   )
 })
