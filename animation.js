0a9da49753e70b100512f956f35acaf957cf4331
fibo <casati_gianluca@yahoo.it>
2018-07-06 10:31:07 +0200

    poem

---
+++ animation.js
@@ -0,0 +1,16 @@
+///////////\
+// Anatema  \_______________
+//                          \.
+// Sulle rive di uno stagno  |_.
+// passò un satiro e la vide,  |
+// Reginella sempre bella      |
+// è la Musica di Euclide.     |.
+//                               \
+// È all'Identità, il riflesso    |
+// ciò a cui diede movimento.     |
+// Ben tre sassi egli lanciò     /
+// distribuendoli nel vento.     |
+//                                \.
+// Poi nell'onda nacque un fiore,  |
+// tre fu il fischio della voce. _/
+/////////////////////////////////
